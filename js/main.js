6ef62b36a2b2a75f06ab1dc903b542511b6f4d59
TRIVEDI <kamiltrivedi@northwesternmutual.com>
2018-02-07 18:42:14 -0600

    resolved

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Initialize Firebase
+
+
+
+// Connect to Database
+
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.046627, lng: -87.900055},
+    zoom: 8
+  });
+}
+
+
+
+
+
