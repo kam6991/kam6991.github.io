be549e01fa0cb33481c0dc887393e2574cfa5c4d
TRIVEDI <kamiltrivedi@northwesternmutual.com>
2018-02-19 19:12:02 -0600

    Final Project

--- js/main.js
+++ js/main.js
@@ -1,27 +1,20 @@
-// Initialize Firebase
 
 
 
-// Connect to Database
 
-/*
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 43.046627, lng: -87.900055},
-    zoom: 8
-
-      });
-}
-*/
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 43.046627, lng: -87.900055},
-      zoom: 10,
-      scrollwheel: false
-    });
-  }
-
-initMap();
+<script src="https://www.gstatic.com/firebasejs/4.10.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCpe2BeYiNJaPaRJ7r5OrU6vxBtkMOQpzQ",
+    authDomain: "study-final-projet.firebaseapp.com",
+    databaseURL: "https://study-final-projet.firebaseio.com",
+    projectId: "study-final-projet",
+    storageBucket: "study-final-projet.appspot.com",
+    messagingSenderId: "373672016230"
+  };
+  firebase.initializeApp(config);
+</script>
 
 
-
+var database = firebase.database();
